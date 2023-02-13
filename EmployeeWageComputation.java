374eb3fee90506fc0a9cbeb8a62b1796ea5eda0a
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-02-13 15:55:12 +0530

    Added: Get total wage when queried by company

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -5,6 +5,7 @@ interface IEmployeeWageComputation
 {
         public void addCompany(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs);
         public void calculateTotalWage();
+        public int getTotalEmpWage(String companyName);
 }
 class CompanyEmpWage
 {
@@ -26,8 +27,7 @@ class CompanyEmpWage
                 this.totalEmpWage = totalEmpWage;
         }
         @Override
-        public String toString()
-        {
+        public String toString() {
                 System.out.println("Details of " + COMPANY_NAME + " employee");
                 System.out.println("-----------------------------------------------------");
                 System.err.println("Wage per hour:" + WAGE_PER_HR);
@@ -51,7 +51,7 @@ class EmployeeWageComputation implements IEmployeeWageComputation
         {
                 CompanyEmpWage company = new CompanyEmpWage(companyName, wagePerHr, maxWorkingDays, maxWorkingHrs);
                 companies.add(company);
-                totalEmpWages.put(companyName,0);
+                totalEmpWages.put(companyName, 0);
         }
         int generateEmployeeType()
         {
@@ -82,7 +82,7 @@ class EmployeeWageComputation implements IEmployeeWageComputation
         {
                 System.out.println("Computation of total wage of " + companyEmpWage.COMPANY_NAME + " employee");
                 System.out.println("-----------------------------------------------------");
-                System.out.printf("%5s     %5s     %5s     %5s\n", "Day", "Workinghrs", "Wage", "Total working hrs");
+                System.out.printf("%4s\t%4s\t%2s\t%4s\n", "Day", "Workinghrs", "Wage", "Total working hrs");
 
                 int workingHrs, totalWage = 0;
                 for (int day = 1, totalWorkingHrs = 0; day <= companyEmpWage.MAX_WORKING_DAYS
@@ -92,19 +92,14 @@ class EmployeeWageComputation implements IEmployeeWageComputation
                         workingHrs = getWorkingHrs(empType);
                         int wage = workingHrs * companyEmpWage.WAGE_PER_HR;
                         totalWage += wage;
-                        System.out.printf("%5d       %5d      %5d      %5d\n", day, workingHrs, wage, totalWorkingHrs + workingHrs);
+                        System.out.printf("%4d\t%5d\t%10d\t%10d\n", day, workingHrs, wage, totalWorkingHrs + workingHrs);
                 }
                 totalEmpWages.put(companyEmpWage.COMPANY_NAME, totalWage);
                 return totalWage;
         }
-        void printTotalEmpWages()
+        public int getTotalEmpWage(String companyName)
         {
-                System.out.println("The Companies and their total Employee Wages are:");
-                for (String companyName : totalEmpWages.keySet())
-                {
-                        System.out.println(companyName + ": " + totalEmpWages.get(companyName));
-                }
-
+                return totalEmpWages.get(companyName);
         }
         public static void main(String args[])
         {
@@ -113,6 +108,8 @@ class EmployeeWageComputation implements IEmployeeWageComputation
                 employeeWageComputation.addCompany("Google", 5, 40, 170);
                 employeeWageComputation.addCompany("Amazon", 19, 10, 150);
                 employeeWageComputation.calculateTotalWage();
-                employeeWageComputation.printTotalEmpWages();
+                String query = "Google";
+                int totalWage = employeeWageComputation.getTotalEmpWage(query);
+                System.out.println("Total Employee Wage for " + query + " company is " + totalWage);
         }
 }
